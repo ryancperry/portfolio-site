9c1b1293938f82f776cff3978a9626d5b9183ac8
Ryan C. Perry <ryanc.perry@MacBookPro.lan>
2025-02-19 11:06:49 -0600

    Add prettier and eslint configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const compat = new FlatCompat();
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended'
+  ),
+  {
+    ignores: [
+      'node_modules/',
+      '.next/',
+      'dist/',
+      'public/',
+      'package-lock.json',
+    ],
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      'react/react-in-jsx-scope': 'off', // Not needed in Next.js
+      'react/prop-types': 'off', // Disable if using TypeScript
+    },
+  },
 ];
-
-export default eslintConfig;
