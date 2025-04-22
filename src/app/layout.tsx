ca6de49cfc45ecbdd3f435897760c657e1a2dab3
ryancperry <perry.ryan.c@gmail.com>
2025-04-22 11:36:33 -0500

    Updated OG image path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
     siteName: 'Ryan C. Perry - Developer',
     images: [
       {
-        url: '/og-image.jpg',
+        url: 'https://ryancperry.com/og-image.jpg',
         width: 1200,
         height: 630,
         alt: "Screenshot of Ryan C. Perry's portfolio website",
